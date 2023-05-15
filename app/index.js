c3a94304bf372099583c5cf4fa7ff9dfb31e54b5
yacine <lentente1958@gmail.com>
2023-05-15 23:12:21 +0100

    --first

--- app/index.js
+++ app/index.js
@@ -1,11 +1,27 @@
 const express=require('express');
-const server=new express();
 
+// require session Managment
+const session=require('express-session');
+const server= express();
+
+// parse incoming POST data
+server.use(express.json())
+server.use(express.urlencoded());
+
+// handle GET Requests
 server.get('/',(req,res)=>{
-const responsedFile="/login/index.html";
+            const sessionStatus=true; // check session open or closed
+            const responsedFile=sessionStatus ? req.path : "/login/index.html";
             res.sendFile(responsedFile,{root:__dirname});
 });
-server.post('/',(req,res)=>{
 
+// handle POST Requests
+server.post('/',(req,res,next)=>{
+   const {Authentication}=require('./_system/Authentication/index')
+   Authentication(req,res,__dirname);
 });
+
+server.get('/error',(req,res)=>{
+    res.send('error 404 ):')
+})
 server.listen(3000);
