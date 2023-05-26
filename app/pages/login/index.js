2c611f9c05acb2dba20c6d902ba6f83fd276ae6e
yacine <lentente1958@gmail.com>
2023-05-26 21:17:34 +0100

    move static pages into pages folder

---
+++ app/pages/login/index.js
@@ -0,0 +1,7 @@
+// import axios from "axios";
+document.getElementById('log-submit').addEventListener('submit',function handler(e){
+    e.preventDefault(); 
+    // const data=new FormData(document.getElementById('log-form'));
+    // const sent=axios.post('/authentication',data);
+    
+});
