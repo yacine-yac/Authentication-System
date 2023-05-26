f1fd685ff461b9d93003c0c417914ec83fe8556d
yacine <lentente1958@gmail.com>
2023-05-26 21:15:13 +0100

    define pageRouting class for handle request routs

---
+++ app/_system/routing/index.js
@@ -0,0 +1,17 @@
+const { existsSync } = require("fs"); 
+
+class pageRouting{
+    constructor(root,erroPage="error.html"){
+        this.root=root;
+        this.errorPage=erroPage;
+    }
+    handle(req,res){
+        
+             const path=req.url; 
+             const check= existsSync(`${this.root}/pages${path}/index.html`)
+                check ? res.sendFile(`pages/${path}/index.html`,{root:this.root})
+                      : res.sendFile(this.errorPage,{root:this.root})
+    }
+    
+} 
+module.exports={pageRouting};
